cdf5276b686d57597c363aec0a6f5e19e559f3d7
alanismirko <alanismirko@gmail.com>
2022-03-22 19:46:34 +0100

    logout with session

--- logout_get.py
+++ logout_get.py
@@ -1,8 +1,43 @@
-from bottle import get, request, redirect
+from bottle import post, request, redirect, get, view
 import g
+import uuid
+import time
+from datetime import datetime
+import mysql.connector
 
 @get("/logout")
 def _():
-    user_session_id = request.get_cookie("uuid4")
-    g.SESSIONS.remove(user_session_id)
+    try:
+###################### VARIABLES #######################################
+        
+        user_session_id = request.get_cookie("uuid4")
+
+###################### CONNECTING TO THE DATABASE ########################
+        db_config = {
+            "host": "localhost",
+            "user":"root",
+            "database": "tweeterdb",
+            "password": "1234"
+            }
+
+        db = mysql.connector.connect(**db_config)
+            
+        cursor = db.cursor(buffered=True)
+        sql = """ DELETE FROM sessions WHERE session_id=%s"""
+
+        cursor.execute(sql, (user_session_id,))
+        db.commit()
+        print("session is deleted", user_session_id )
+
+
+    except Exception as ex:
+        print(ex)
+    finally:
+        db.close()
+
+###################### RETURN ########################
     return redirect("/login")
+
+
+
+
